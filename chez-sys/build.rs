11cb95b63693f9bb6e3abc8def9d92932773ead4
Niklas Larsson <niklas@mm.st>
2026-04-17 23:13:44 +0200

    Continue with build script

--- chez-sys/build.rs
+++ chez-sys/build.rs
@@ -1,24 +1,52 @@
-use std::env;
+use std::env::{self, current_dir, set_current_dir};
+use std::io::{Error, Result};
+use std::path::Path;
 
-
-fn main() {
-    let platform = get_platform();
-    if cfg!(windows) {
-        build_windows();
+fn main() -> Result<()> {
+    let system_lib = env::var("CARGO_FEATURE_SYSTEM").is_ok();
+    if !system_lib {
+        build()?;
     } else {
-        build_unix();
+        // Link to system lib
     }
+    Ok(())
 }
 
-fn build_windows() {
-
+fn build() -> Result<()> {
+    check_chez_dir()?;
+    let platform = get_platform()?;
+    let target_env = env::var("CARGO_CFG_TARGET_ENV").unwrap_or_default();
+    let debug = env::var("DEBUG").is_ok();
+    let config = BuildConfig::new_config(platform, debug);
+    if cfg!(windows) && target_env != "gnu" {
+        build_windows(&config)?;
+    } else {
+        build_unix(&config)?;
+    }
+    Ok(())
 }
 
-fn build_unix() {
+fn check_chez_dir() -> Result<()> {
+    let path = Path::new("ChezScheme");
+    if !path.exists() {
+        println!("cargo::error=No ChezScheme directory. Check out the git submodule.");
+        return Err(Error::new(std::io::ErrorKind::NotFound, "ChezScheme not found"));
+    }
+    set_current_dir(path)?;
+    Ok(())
+}
+
+fn build_windows(config : &BuildConfig) -> Result<()> {
     
+
+    Ok(())
 }
 
-fn get_platform() -> String {
+fn build_unix(config : &BuildConfig) -> Result<()> {
+    Ok(())
+}
+
+fn get_platform() -> Result<String> {
     let os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let cpu = match arch.as_str() {
@@ -35,7 +63,18 @@ fn get_platform() -> String {
         "freebsd" => "fb",
         "netbsd" => "nb",
         "openbsd" => "ob",
-        _ => ""   
+        _ => return Err(Error::new(std::io::ErrorKind::Unsupported, "Unsupported OS"))
     };
-    format!("t{}{}",cpu, pl)
+    Ok(format!("t{}{}",cpu, pl))
+}
+
+pub struct BuildConfig {
+    pub platform:String,
+    pub debug:bool
+}
+
+impl BuildConfig {
+    pub fn new_config(platform : String, debug:bool) -> Self {
+        BuildConfig { platform, debug }
+    }
 }
